7e23fdb6e231786e6cab5a2b952ff8fedc573220
sraina <sraina@uptycs.com>
2020-03-29 15:32:04 +0530

    first spring project

---
+++ src/main/java/sushrut/springdipoc/springdi/SpringDiApplication.java
@@ -0,0 +1,13 @@
+package sushrut.springdipoc.springdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDiApplication.class, args);
+	}
+
+}
